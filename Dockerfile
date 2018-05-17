a426fc6de5abd30363dc8c04840f7a01d802a027
Mahmoud Abdelkader <mabdelkader@gmail.com>
2018-05-17 06:09:54 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3-alpine3.7
+
+# install console and node
+RUN apk update &&\
+    apk add --no-cache \
+        bash \
+        socat \
+    mkdir -p /opt/app
+
+# install pip ( in separate dir due to docker cache)
+ADD setup.py /opt/app
+RUN cd /opt/app && pip install -e .
+
+WORKDIR /opt/app/src
+ADD  . /opt/app/src
+VOLUME /opt/app/src
+
+# this is for virtual host purposes
+EXPOSE 3000
+CMD ["bash"]
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import setup
+
+
+VERSION = '0.1.0'
+
+
+setup(
+    name='test'
+    version=VERSION,
+    author=', '.join([
+        'Mahmoud Abdelkader',
+    ]),
+    packages=find_packages(exclude=['tests', '*.test', '*.test.*']),
+    include_package_data=True,
+    zip_safe=False
+    install_requires=[
+        'attrs>=18.0,<19',
+        'flask'
+    ],
+    license='MIT'
+)
